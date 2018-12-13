e9e1f665a70a9cd6a6e7a4ecee10c72dbfb41ebc
nemanjan00 <nemanjan00@gmail.com>
2018-12-13 07:07:53 +0100

    Refactor

--- HTTPClientTask.php
+++ HTTPClientTask.php
@@ -48,3 +48,4 @@ class HTTPClientTask extends Task{
         }
     }
 }
+
---
+++ TaskMessageRouter.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miljanrakita
+ * Date: 12/13/18
+ * Time: 4:37 AM
+ */
+
+namespace App;
+
+
+class TaskMessageRouter {
+    private static $taskMessageRouter;
+    private static $webHookHandler;
+
+    public static function getTaskMessageRouter(){
+        if(!self::$taskMessageRouter) {
+            self::$taskMessageRouter = new TaskMessageRouter();
+
+            return self::$taskMessageRouter;
+        } else {
+            return self::$taskMessageRouter;
+        }
+    }
+
+    public static function subscribe($name, TaskMessageHandler $handler){
+        $taskMessageRouter = self::getTaskMessageRouter();
+        $taskMessageRouter->handlers[$name] = $handler;
+    }
+
+    public static function call($name, $task, $params){
+        $taskMessageRouter = self::getTaskMessageRouter();
+
+        $handler = $taskMessageRouter->getHandlerByName($name);
+        $handler->handle($task, $params);
+    }
+
+    private static function getHandlerByName($name)
+    {
+        $taskMessageRouter = self::getTaskMessageRouter();
+
+        if(isset($taskMessageRouter->handlers[$name])) {
+            return $taskMessageRouter->handlers[$name];
+        } else {
+            throw Exception("Handler for $name is not registered. ");
+        }
+    }
+
+    private $handlers = [];
+
+    private function __construct()
+    {
+    }
+}
+
