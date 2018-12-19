7745b257ea8ee12003fdfe950004dff2f0e6fb7d
nemanjan00 <nemanjan00@gmail.com>
2018-12-19 04:41:39 +0100

    Fix mysql id

--- src/WorkerJS/PHPClient/MySQLTaskStore.php
+++ src/WorkerJS/PHPClient/MySQLTaskStore.php
@@ -20,7 +20,7 @@
     }
 
     public function getTask($taskID){
-        $result = mysqli_query($this->connection, "SELECT `task` FROM `tasks` WHERE `taskID` = ".intval($taskID));
+        $result = mysqli_query($this->connection, "SELECT `task` FROM `tasks` WHERE `taskID` = '".mysqli_real_escape_string($this->connection, $taskID)."'");
 
         if(mysqli_num_rows($result) === 0){
             throw new \Exception("Task $taskID not found.");
@@ -31,6 +31,6 @@
     }
 
     public function setTask($taskID, Task $task){
-        mysqli_query($this->connection, "INSERT INTO `tasks` (`taskID`, `task`) VALUES (".intval($taskID).", '".mysqli_real_escape_string($this->connection, json_encode($task->getTask()))."')");
+        mysqli_query($this->connection, "INSERT INTO `tasks` (`taskID`, `task`) VALUES ('".mysqli_real_escape_string($this->connection, $taskID)."', '".mysqli_real_escape_string($this->connection, json_encode($task->getTask()))."')");
     }
  }
